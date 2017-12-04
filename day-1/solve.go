33b68dd94ae2eb7233620dfef0061eb084dedf7b
Hugh Davenport <hugh@davenport.net.nz>
2017-12-04 17:24:08 +1300

    Solved part 2

--- day-1/solve.go
+++ day-1/solve.go
@@ -11,23 +11,25 @@ func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: ")
 		fmt.Println()
-		fmt.Println(os.Args[0] + " <file>")
+		fmt.Println(os.Args[0] + " <file> [part2]")
 		os.Exit(1)
 	}
-	data, err := ioutil.ReadFile(os.Args[1])
+	filename := os.Args[1]
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(2)
 	}
 	captcha := strings.TrimSpace(string(data))
+	gap := 1
+	if len(os.Args) >= 3 {
+		gap = len(captcha) / 2
+	}
 	sum := 0
-	for i, c := range captcha[:len(captcha)-1] {
-		if captcha[i+1] == captcha[i] {
+	for i, c := range captcha {
+		if captcha[(i+gap)%len(captcha)] == captcha[i] {
 			sum += int(c) - int('0')
 		}
-	}
-	if captcha[0] == captcha[len(captcha)-1] {
-		sum += int(captcha[0]) - int('0')
 	}
 
 	fmt.Println(sum)
