938745e18bdfc35fbd051b7dceada04afc192041
Hugh Davenport <hugh@davenport.net.nz>
2017-12-28 18:25:43 +1300

    Solved day 3 part 2 in google sheets

---
+++ day-3/solve2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	//"math"
+	//"os"
+	//"strconv"
+)
+
+func main() {
+	//if len(os.Args) < 2 {
+	//	fmt.Println("Usage: ")
+	//	fmt.Println()
+	//	fmt.Println(os.Args[0] + " <input>")
+	//	os.Exit(1)
+	//}
+	//input, err := strconv.Atoi(os.Args[1])
+	//if err != nil {
+	//	fmt.Println("Error: ", err)
+	//	os.Exit(2)
+	//}
+	//if input == 1 {
+	//	fmt.Println(0)
+	//	return
+	//}
+	// bottom corner is odd number squared incremented, so square root
+	//ringEnd := 1
+	//ringIndex := 1
+	//for ringEnd < input {
+	//	ringIndex += 1
+	//	ringEnd += 4 * (ringIndex - 1) * 2
+	//}
+	//ringStart := ringEnd - 4 * (ringIndex  - 1) * 2 + 1
+	//side := (ringEnd - ringStart + 1) / 4 + 1
+	//input -= ringStart - 1
+	//input %= side - 1
+	//mid := side / 2
+
+	//distance := ringIndex - 1 + int(math.Abs(float64(input - mid)))
+	//fmt.Println(distance)
+	// https://docs.google.com/spreadsheets/d/1Dr4Xcf4SJFRV9TpUM-MKublFnFVDNJTIk_1AtBJ4ab0/edit#gid=0
+	fmt.Println(266330)
+}
