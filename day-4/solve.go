5ba3479fbfbda69889c38e4e20ed03f88d09c1d6
Hugh Davenport <hugh@davenport.net.nz>
2017-12-28 18:55:07 +1300

    Add part 2 for day 4

--- day-4/solve.go
+++ day-4/solve.go
@@ -4,14 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
+
+type runeArray []rune
+
+func (s runeArray) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s runeArray) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s runeArray) Len() int {
+	return len(s)
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: ")
 		fmt.Println()
-		fmt.Println(os.Args[0] + " <file>")
+		fmt.Println(os.Args[0] + " <file> [part2]")
 		os.Exit(1)
 	}
 	filename := os.Args[1]
@@ -21,6 +36,10 @@ func main() {
 		os.Exit(2)
 	}
 	defer file.Close()
+	sortChars := false
+	if len(os.Args) == 3 {
+		sortChars = true
+	}
 
 	scanner := bufio.NewScanner(file)
 	sum := 0
@@ -29,6 +48,11 @@ func main() {
 		set := make(map[string]bool)
 		valid := true
 		for _, word := range words {
+			if sortChars {
+				runes := []rune(word)
+				sort.Sort(runeArray(runes))
+				word = string(runes)
+			}
 			if set[word] {
 				valid = false
 				break
